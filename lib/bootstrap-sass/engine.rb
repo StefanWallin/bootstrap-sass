0423f8c1fdb9fb5454bb8834de9188f582032fbc
Stefan Wallin <stefan.w@festiz.com>
2020-01-17 13:38:18 +0100

    Attempt #1

--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -6,10 +6,17 @@ module Bootstrap
           app.config.assets.paths << root.join('assets', sub).to_s
         end
 
-        # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
-        # and automatically precompiles the referenced assets.
-        unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
+
+        sprockets_version = Sprockets::Rails::VERSION.split('.', 2)[0].to_i
+        unless sprockets_version == 3
+          # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
+          # and automatically precompiles the referenced assets.
           app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff2?)$)
+        end
+
+        unless sprockets_version == 4
+          # sprockets-rails 4 no longer support regexes, only wildcards
+          app.config.assets.precompile << ["bootstrap/glyphicons-halflings-regular.*"]
         end
       end
     end
